8bc8cb3a688e1c2e67aeb7fe649ee00d642f9141
Pietro-Leone <leonepietro1999@gmail.com>
2023-07-11 16:32:49 +0200

    fine consegna base

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 'use strict'
 
+const container = document.getElementById("block-container");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+
+    container.innerHTML = "";
+
+    for (let i = 0; i < 100; i++) {
+
+        let block = document.createElement("div");
+        block.classList.add("block");
+        block.innerHTML = i.toString();
+        container.append(block);
+
+        block.addEventListener("click", function () {
+            block.classList.toggle("bg-success");
+        })
+    }
+
+})
