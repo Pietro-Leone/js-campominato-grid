7ca36839aa407b5d9ba1884ba6fd49a1c6759a37
Pietro-Leone <leonepietro1999@gmail.com>
2023-07-11 17:47:46 +0200

    fine esercizio e bonus

--- js/main.js
+++ js/main.js
@@ -1,22 +1,47 @@
 'use strict'
 
 const container = document.getElementById("block-container");
+const select = document.getElementById("difficulty");
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", function () {
 
+    const selectDifficulty = parseInt(select.value);
+
+    const difficulty = selectRow(selectDifficulty);
+
     container.innerHTML = "";
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < difficulty; i++) {
 
-        let block = document.createElement("div");
-        block.classList.add("block");
-        block.innerHTML = i.toString();
-        container.append(block);
+        createBlock(container, difficulty);
 
-        block.addEventListener("click", function () {
-            block.classList.toggle("bg-success");
-        })
     }
 
 })
+
+function createBlock(containerBlock, y) {
+    let block = document.createElement("div");
+    block.classList.add("block");
+    block.style.flexBasis = `calc(100% / ${Math.sqrt(y)})`;
+    containerBlock.append(block);
+
+    block.addEventListener("click", function () {
+        block.classList.toggle("bg-success");
+    })
+}
+
+function selectRow(x) {
+    let blockInRow = 0;
+
+    if (x === 1) {
+        blockInRow = 100;
+    }
+    else if (x === 2) {
+        blockInRow = 81;
+    }
+    else if (x === 3) {
+        blockInRow = 49;
+    }
+    return blockInRow;
+}
